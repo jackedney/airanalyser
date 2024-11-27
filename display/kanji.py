1a35ff569b8d6c0262659b39013913ea8e62bb64
jack.edney <jack.edney@outlook.com>
2024-11-27 23:25:23 +0000

    First commit

---
+++ display/kanji.py
@@ -0,0 +1,41 @@
+from display.demo_opts import get_device
+from luma.core.render import canvas
+from PIL import Image, ImageDraw, ImageFont
+
+FONT_PATH = "display/hkhigerei/hkhigerei.ttf"
+
+
+def display_kanji(
+    kanji: str, font_size: int, image_width: int, image_height: int
+) -> Image:
+    kanji = kanji[0]
+    pil_font = ImageFont.truetype(FONT_PATH, size=font_size, encoding="unic")
+    text_width, text_height = pil_font.getbbox(kanji)[2:]
+    im_canvas = Image.new("RGB", [font_size, font_size], (255, 255, 255))
+
+    draw = ImageDraw.Draw(im_canvas)
+    offset = ((font_size - text_width) // 2, (font_size - text_height) // 2)
+    white = "#000000"
+    draw.text(offset, kanji, font=pil_font, fill=white)
+
+    im_canvas.resize((image_width, image_height), Image.Resampling.LANCZOS)
+    return im_canvas
+
+
+def main():
+    cols = device.width
+    rows = device.height
+
+    while True:
+        with canvas(device, dither=True) as draw:
+            kanji = input("Enter Kanji:")
+            kanji_image = display_kanji(kanji, 25, cols, rows)
+            draw.display(kanji_image)
+
+
+if __name__ == "__main__":
+    try:
+        device = get_device()
+        main()
+    except KeyboardInterrupt:
+        pass
